d4d6d07399622c08b898948601ace50009036291
HullPerse <alex31054@yandex.ru>
2024-03-11 00:59:58 +0300

    ADDED: reset claim countdown

--- src/components/mudae_navbar/MudaeCountdown.tsx
+++ src/components/mudae_navbar/MudaeCountdown.tsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 
 export default function MudaeCountdown() {
   const [time, setTime] = useState({ minutes: 0, seconds: 0 });
+  const [resetTime, setResetTime] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,12 +32,57 @@ export default function MudaeCountdown() {
     return () => clearInterval(interval);
   });
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const currentMinutes = currentTime.getMinutes();
+      const currentSeconds = currentTime.getSeconds();
+
+      let remainingHours = 3 - currentTime.getHours();
+      let remainingMinutes = 57 - currentMinutes;
+      let remainingSeconds = 60 - currentSeconds;
+
+      if (remainingHours < 0) {
+        remainingHours = 0;
+        remainingMinutes = 57 - currentMinutes;
+        remainingSeconds = 60 - currentSeconds;
+      }
+
+      if (remainingSeconds < 0) {
+        remainingHours -= 1;
+        remainingMinutes -= 1;
+        remainingSeconds = 60 + remainingSeconds;
+      }
+
+      if (remainingMinutes < 0) {
+        remainingMinutes = 57;
+      }
+
+      setResetTime({
+        hours: remainingHours,
+        minutes: remainingMinutes,
+        seconds: remainingSeconds,
+      });
+    });
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="font-bold">Время до роллов: </h1>
-      <p>{`${time.minutes.toString().padStart(2, "0")}:${time.seconds
-        .toString()
-        .padStart(2, "0")}`}</p>
+    <div className="flex flex-row justify-center items-center gap-x-4">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold">Время до роллов: </h1>
+        <p>{`${time.minutes.toString().padStart(2, "0")}:${time.seconds
+          .toString()
+          .padStart(2, "0")}`}</p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold">Время до клейма: </h1>
+        <p>{`${resetTime.hours.toString()}:${resetTime.minutes
+          .toString()
+          .padStart(2, "0")}:${resetTime.seconds
+          .toString()
+          .padStart(2, "0")}`}</p>
+      </div>
     </div>
   );
 }
