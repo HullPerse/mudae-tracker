54a1581b0f737a878d08b315b60d27990d5c5267
HullPerse <alex31054@yandex.ru>
2024-03-12 04:51:40 +0300

    REMOVED: sonner

--- src/components/mudae_ui/mudae_navbar/Mudae_Form.tsx
+++ src/components/mudae_ui/mudae_navbar/Mudae_Form.tsx
@@ -35,6 +35,8 @@ export default function MudaeForm() {
         await loginUser(username, password).then(() => {
           localStorage.setItem("user", username);
           setUser(username);
+
+          window.location.reload();
         });
       });
     }
